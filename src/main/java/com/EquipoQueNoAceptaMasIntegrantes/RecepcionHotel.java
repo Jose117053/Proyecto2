61e04cad483e3522fdd6ba63038c27b3ef844b50
Jose117053 <adolfo19420@gmail.com>
2023-11-13 19:55:21 -0600

    Interfaz grafica removida

--- src/main/java/com/EquipoQueNoAceptaMasIntegrantes/RecepcionHotel.java
+++ src/main/java/com/EquipoQueNoAceptaMasIntegrantes/RecepcionHotel.java
@@ -8,16 +8,9 @@ import com.EquipoQueNoAceptaMasIntegrantes.Vista.HotelFacade;
 
 public class RecepcionHotel {
     public static void main(String[] args) throws IOException {
-        ////////////////////////////////////////
-        // VistaIdioma vista=new VistaIdioma();
-        // Properties msg = new Properties();
-        // ControladorIdioma controlador = new ControladorIdioma(vista, msg);
-        // vista.setVisible(true);
-        //////////////////////////////////////
         RepositorioOferta repositorioOferta = RepositorioOferta.getInstance();
         RepositorioHabitacion repositorioHabitacion = RepositorioHabitacion.getInstance();
         GeneradorOfertas generadorOfertas = new GeneradorOfertas(repositorioOferta, repositorioHabitacion);
-
         HotelFacade hotelFacade = new HotelFacade(repositorioOferta, repositorioHabitacion, generadorOfertas);
 
         hotelFacade.reservar();
